06f1ad79e9dc1c5e3f3f320d7d5408e2b4865af3
John-Wambua <jwambua307@gmail.com>
2020-09-21 23:47:03 +0300

    user authentication modification

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,10 @@
-const User = require ("../models/user");
+const User = require ("../models/userModel");
 const AppError =  require ("../utils/appError")
 const catchAsync = require('../utils/catchAsync')
+const sendEmail = require('../utils/email')
+const _ = require('lodash');
+const crypto=require('crypto');
+
 exports.signup = catchAsync(async(req,res,next)=>{
         const user = await User.create({
             name: req.body.name,
@@ -25,8 +29,91 @@ exports.signup = catchAsync(async(req,res,next)=>{
    //user exists
    const user = await User.findOne({email}).select("+password")
    
-    if(!user||!await user.verifyPassword(password,user.password,next)) return next(new AppError("Incorrect email or password", 401))
+    if(!user||!await user.verifyPassword(password,user.password,next)) {
+        return next(new AppError("Incorrect email or password", 401))
+    }
 
     user.generateAuthToken(200, res);
 
  })
+
+exports.forgotPassword = catchAsync(async (req,res,next)=>{
+    // 1) Get user based on posted email
+    const {email} = req.body;
+
+    const user = await User.findOne({email});
+
+    if (!user) return next(new AppError('There is no user with the specified email',404));
+
+    //2) Generate random reset token
+    const resetToken = user.createPasswordResetToken();
+    await user.save({validateBeforeSave: false})
+
+    //3) Send Token to user through Email
+
+    try{
+        const resetURL=`${req.protocol}://${req.get(
+            'host'
+        )}/api/v1/users/resetPassword/${resetToken}`;
+
+        const message = `Forgot your password? Reset it at ${resetURL}.\n
+         If you did not initiate this process, please ignore this email`;
+
+        await sendEmail({
+            email,
+            subject: 'Your password reset token (valid for 10 minutes)',
+            message
+        })
+        /*TODO send token to user's email */
+        res.status(200).json({
+            status: 'success',
+            message: 'Token sent to email'
+        })
+    }catch (e) {
+        user.passwordResetToken=undefined;
+        user.passwordResetExpires=undefined;
+
+        await user.save({validateBeforeSave:false});
+        return next(new AppError('There was an error sending the email.Try again later!',500));
+    }
+
+})
+
+exports.resetPassword=catchAsync(async (req,res,next)=>{
+    // 1) get user based on the token
+    const hashedToken=crypto.createHash('sha256').update(req.params.token).digest('hex');
+    const user=await User.findOne({passwordResetToken:hashedToken,passwordResetExpires:{$gt:Date.now()}})
+
+    // 2) If token has not expires and there is user, set the new password
+    if (!user) return next(new AppError(new Error('Token is invalid or has expired'),400))
+    user.password=req.body.password;
+    user.passwordConfirm=req.body.passwordConfirm;
+    user.passwordResetToken=undefined;
+    user.passwordResetExpires=undefined;
+
+    await user.save();
+
+    //3) Update changedPasswordAt property for the user
+
+    //4) Log user in, send JWT
+    user.generateAuthToken(200,res);
+
+})
+
+//USER HAS TO BE LOGGED IN TO UPDATE PASSWORD
+exports.updatePassword=catchAsync(async (req,res,next)=>{
+    // 1) Get User
+    if (!req.user) return next(new AppError('You are not logged in',401));
+    const user=await User.findById(req.user._id).select('+password');
+    // 2) Check if posted password is correct
+    console.log(user);
+    if (!await user.verifyPassword(req.body.password,user.password,next)) return next(new AppError('Incorrect Password!',401));
+
+    // 3) Update password
+    user.password=req.body.newPassword;
+    user.passwordConfirm=req.body.newPasswordConfirm;
+    await user.save()
+    // 4) Log in user.
+    user.generateAuthToken(200,res);
+
+})
---
+++ models/userModel.js
@@ -0,0 +1,174 @@
+
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
+const _ = require('lodash');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const AppError = require("../utils/appError");
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required:[true, "Please provide your name"],
+        minlength:5,
+        maxlength:50,
+    },
+    phonenumber: {
+        type:Number,
+        /*TODO provide phone number validation*/
+        //validate: [validator.isMobilePhone, "Please enter a valid phone number"],
+        required:[true, "Please enter a valid phone number"]
+        
+    },
+    email: {
+        type:String,
+        required:[true, "Please provide your email"],
+        unique:true,
+        lowercase:true,
+        validate: [validator.isEmail, "Please provide a valid email"]
+    },
+    option:{
+        type:String,
+        enum: ["tutor","student"],
+        required:[true, "Please select an option"]
+    },
+    subjects:[
+        {
+            name:{
+                type:String,
+                required:[true,"Please enter your subject"],
+            },
+            rate:{
+                type:Number,
+                required:[true,"Please enter hourly rate"]
+            },
+            
+        }
+    ],
+    password:{
+        type:String,
+        required:[true, "Please input your password"],
+        minlength:8,
+        select: false
+    },
+    passwordConfirm:{
+        type:String,
+        required:[true, "Please confirm the password"],
+        validate: function(val){
+            //return t or f
+            return this.password === val;
+        },
+        message: "Passwords do not match",
+        
+    },
+    photo:{
+        type: String,
+        default: "default.png"
+    },
+    role: {
+        type:String, 
+        enum: ["admin", "user"],
+        default: "user"
+    },
+
+    passwordChangedAt:Date,
+    passwordResetToken:String,
+    passwordResetExpires:Date,
+    active:{
+        type:Boolean,
+        default: true,
+        select:false
+    }
+    
+});
+
+userSchema.pre('save',function (next){
+    if (!this.isModified('password')||this.isNew) return next();
+
+    this.passwordChangedAt = Date.now()-1000;
+    next();
+})
+
+//executes upon password modified
+userSchema.pre("save",async function(next){
+    try{
+        if(!this.isModified('password')) return next() 
+
+       this.password= await bcrypt.hash(this.password, saltRounds);
+       this.passwordConfirm = undefined;
+       next()
+
+    }catch(e){
+        //with a parameter assumes error 
+        next(e)
+    }
+
+}) 
+/*TODO validation- only tutors can input and update subjects*/
+// userSchema.pre("findOneAndUpdate",async function(next){
+
+// if (this.getUpdate().$set.subjects) {
+//     if(this.option !== "tutor"){
+//         return next(new AppError("Only tutors can add subjects", 401))
+//     }else{
+//         return next()
+//     }
+// }else{
+//     return next()
+// }
+// }) 
+
+
+
+//create a fxn called authgenerate token 
+userSchema.methods.generateAuthToken= function(statusCode,res){
+    
+        const token = jwt.sign({ id: this._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXP })
+    
+        return res.status(statusCode).json({
+            status: "success",
+            token,
+            data: {
+                user: _.omit(this.toObject(),['password','passwordChangedAt','__v','active'])
+            }
+        })
+}
+
+userSchema.methods.verifyPassword = async function (candidatePassword, dbPassword,next){
+    
+    try{
+       return await bcrypt.compare(candidatePassword, dbPassword)
+    }
+    catch(e){
+        next(e)
+    }
+
+}
+//- --Check if password was changed after JWT was issued
+userSchema.methods.changedPasswordAfter =function (JWTTimestamp){
+    if (this.passwordChangedAt){
+        const changedTimestamp=parseInt(this.passwordChangedAt.getTime()/1000,10);
+        return JWTTimestamp<changedTimestamp;//if true, password was changed
+    }
+    return false;
+}
+
+// --Create password reset token for resetting user password
+userSchema.methods.createPasswordResetToken = function (){
+    const resetToken = crypto.randomBytes(32).toString('hex');
+
+    this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+    // console.log({ resetToken }, this.passwordResetToken);
+    return resetToken;
+}
+
+
+
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
+
+   
